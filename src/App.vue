7ffcfb1be54095b1a1e8c53aa356ddf3a3d1d54b
Beeee0927 <1572255051@qq.com>
2024-10-26 17:57:11 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { mref } from '@/../packages/lib/storeProxy'
+import { ref } from 'vue'
+
+// type A = {
+//   a: 1
+//   b: 2
+// }
+// type B<T> = {
+//   [key in keyof T as key extends 'a' ? never : key]: key
+// }
+
+// const b: B<{
+//   a: string
+//   b: string
+// }> = {
+//   a: '2',
+//   b: 123
+// }
+
+const x = mref({
+  // value: false,
+  switch() {
+    console.log('ok')
+    this.value = !this.value
+  }
+})
+</script>
+
+<template>
+  App
+  <button @click="console.log(x.value)">{{ x }}</button>
+</template>
+
+<style scoped></style>
